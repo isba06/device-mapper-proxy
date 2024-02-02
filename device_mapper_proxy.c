6bdc9a9cfeef90cfdedd3ad4dc609cfb3070b08e
Ismail <www.ismail.com@mail.ru>
2024-02-02 17:53:00 +0300

    change logfile name, other fixes

--- device_mapper_proxy.c
+++ device_mapper_proxy.c
@@ -8,131 +8,143 @@
 
 #define DM_MSG_PREFIX "dmp"
 
-struct dm_proxy {
-        struct dm_dev *dev;
-        sector_t start;
-};
-
 struct statistics {
-	unsigned long long read_count;
-	unsigned long long write_count;
-	unsigned long long average_read_count;
-	unsigned long long average_write_count;
-	unsigned long long total_requests;
-	unsigned long long block_average_size;
+        unsigned long long read_count;
+        unsigned long long write_count;
+        unsigned long long avg_read_blck;
+        unsigned long long avg_write_blck;
+        unsigned long long total_requests;
+        unsigned long long avg_size_blck;
 };
 
 struct statistics stats = {
-	.read_count = 0,
+        .read_count = 0,
         .write_count = 0,
-        .average_read_count = 0,
-        .average_write_count = 0,
-        .total_requests = 0,
-        .block_average_size = 0,
+        .avg_read_blck = 0,
+        .avg_write_blck = 0,
+        .avg_size_blck = 0,
 };
 
 static int dmp_map(struct dm_target *ti, struct bio *bio)
 {
-        struct dm_proxy *dmproxy = (struct dm_proxy *) ti->private;
+        printk(KERN_DEBUG "\ndmp: start map\n");
+		
+        struct dm_dev * device = (struct dm_dev *) ti->private;
+        bio_set_dev(bio, device->bdev);
 
-	switch(bio_op(bio)) {
-	case REQ_OP_READ:
-		stats.read_count++;
-		break;
-	case REQ_OP_WRITE:
-		stats.write_count++;
-		break;
-	}
+        switch(bio_op(bio)) {
+        case REQ_OP_READ:
+                ++stats.read_count;
+                if(stats.avg_read_blck == 0)
+                        stats.avg_read_blck = bio->bi_iter.bi_size;
+                else
+                        stats.avg_read_blck = (stats.avg_read_blck * stats.read_count + bio->bi_iter.bi_size) / stats.read_count;
+                break;
+
+        case REQ_OP_WRITE:
+                ++stats.write_count;
+				if(stats.avg_write_blck == 0)
+                    	stats.avg_write_blck = bio->bi_iter.bi_size;
+                else
+                    	stats.avg_write_blck = (stats.avg_write_blck * stats.write_count + bio->bi_iter.bi_size) / stats.write_count;
+                break;
+
+        default:
+                return DM_MAPIO_KILL;
+        }
 
         stats.total_requests = stats.read_count + stats.write_count;
-
-	bio_set_dev(bio, dmproxy->dev->bdev);
-       	bio_endio(bio);
-
-        return DM_MAPIO_SUBMITTED;
+		stats.avg_size_blck = (stats.read_count * stats.avg_read_blck + stats.write_count * stats.avg_write_blck) / (stats.read_count + stats.write_count);
+        printk(KERN_DEBUG "\ndmp: end map\n");
+        return DM_MAPIO_REMAPPED;
 }
 
 
 static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 {
-        struct dm_proxy *dmproxy;
-        unsigned long long start;
+        printk(KERN_DEBUG "\ndmp: start constructor\n");
+        struct dm_dev * device;
 
         if (argc != 1) {
-                printk(KERN_CRIT "\n Invalid number of arguments.\n");
+                printk(KERN_CRIT "\nInvalid number of arguments.\n");
                 ti->error = "Invalid argument count";
                 return -EINVAL;
         }
 
-        dmproxy->start = (sector_t)start;
+        device = kmalloc(sizeof(*device), GFP_KERNEL);
 
-        if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &(dmproxy->dev))) {
-                ti->error = "dm-proxy: Device lookup failed";
+        if(device==NULL) {
+                ti->error = "ERROR: Cannot allocate";
+                printk(KERN_CRIT "\ndm: Cannot allocate\n");
+                return -ENOMEM;
+        }
+
+        if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &(device))) {
+                ti->error = "dmp: Device lookup failed";
+                kfree(device);
                 return -EINVAL;
         }
 
-        ti->private = dmproxy->dev;
+        ti->private = device;
+        printk(KERN_DEBUG "\ndmp: last constructed\n");
         return 0;
 }
 
 
 static void dmp_dtr(struct dm_target *ti)
 {
-  struct dm_proxy *dmproxy = (struct dm_proxy *)ti->private;
-  dm_put_device(ti, dmproxy->dev);
-  printk(KERN_CRIT "\n>>destructed\n");
+  struct dm_dev *device= (struct dm_dev *)ti->private;
+  dm_put_device(ti, device);
+  printk(KERN_DEBUG "\ndmp: destructed\n");
 }
 
 
 static struct target_type dmp_target = {
-	.name = "dmp",
-	.version = {1,0,0},
-	.module = THIS_MODULE,
-	.ctr = dmp_ctr,
-	.dtr = dmp_dtr,
-	.map = dmp_map,
+        .name = "dmp",
+        .version = {2,0,0},
+        .module = THIS_MODULE,
+        .ctr = dmp_ctr,
+        .dtr = dmp_dtr,
+        .map = dmp_map,
 };
 
-static struct kobject* dmpstats_kobj;
-
-static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) 
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-        return sprintf(buf, "\nread\n requests:%lld\n avg size:%lld\nwrite\n requests:%lld\n avg size:%lld\ntotal\n requests:%lld\n avg size:%lld\n", 
-                stats.read_count, stats.average_read_count, stats.write_count, stats.average_write_count, stats.total_requests, stats.block_average_size);
+        return sprintf(buf, "\nread\n requests:%lld\n avg size:%lld\nwrite\n requests:%lld\n avg size:%lld\ntotal\n requests:%lld\n avg size:%lld\n",
+                stats.read_count, stats.avg_read_blck, stats.write_count, stats.avg_write_blck, stats.total_requests, stats.avg_size_blck);
 }
+static struct kobject* dmpstats_kobj;
+struct kobj_attribute dmpstats_attr = __ATTR(volumes, 0660, sysfs_show, NULL);
 
-struct kobj_attribute dmpstats_attr = __ATTR(dmpstats, 0660, sysfs_show, NULL);
-
-static int __init dmp_init(void) 
+static int __init dmp_init(void)
 {
+        printk(KERN_DEBUG "\ndmp: start init\n");
         struct kobject mod_ko = (((struct module*)(THIS_MODULE))->mkobj).kobj;
-	dmpstats_kobj = kobject_create_and_add("stat", &mod_ko);
-	if (!dmpstats_kobj)
-	{
-		DMERR("creating stat directory failed");
-		return -ENOMEM;
-	}
-	if (sysfs_create_file(dmpstats_kobj, &dmpstats_attr.attr))
-	{
-		sysfs_remove_file(dmpstats_kobj, &dmpstats_attr.attr);
-		kobject_put(dmpstats_kobj);
-		return -1;
-	}
+        dmpstats_kobj = kobject_create_and_add("stat", &mod_ko);
+        if (!dmpstats_kobj) {
+                printk(KERN_CRIT "\ndmp: creating stat directory failed\n");
+                return -ENOMEM;
+        }
+        if (sysfs_create_file(dmpstats_kobj, &dmpstats_attr.attr)) {
+                sysfs_remove_file(dmpstats_kobj, &dmpstats_attr.attr);
+                kobject_put(dmpstats_kobj);
+                return -1;
+        }
 
         int r = dm_register_target(&dmp_target);
         if (r < 0)
-	        DMERR("register failed %d", r);
-
-	return r;
+                printk(KERN_CRIT "\ndmp: register failed %d\n", r);
+        printk(KERN_DEBUG "\ndmp: init end\n");
+        return r;
 }
 
 static void __exit dmp_exit(void)
 {
-        dm_unregister_target(&dmp_target);  
-	sysfs_remove_file(dmpstats_kobj, &dmpstats_attr.attr);
-	kobject_put(dmpstats_kobj);
-  
-        DMINFO("dmp is removed from the kernel\n");
+        printk(KERN_DEBUG "\ndmp: start exit\n")
+        dm_unregister_target(&dmp_target);
+        sysfs_remove_file(dmpstats_kobj, &dmpstats_attr.attr);
+        kobject_put(dmpstats_kobj);
+        printk(KERN_DEBUG "\ndmp: exit end\n");
         return;
 }
 
@@ -140,5 +152,3 @@ module_init(dmp_init)
 module_exit(dmp_exit)
 
 MODULE_LICENSE("GPL");
-
-
